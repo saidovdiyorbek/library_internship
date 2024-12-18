f7e930448659a796137205ab5c231dbaa6dc6610
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-18 18:54:48 +0500

    I don't now

---
+++ src/test/java/intership/libraryintership/LibraryIntershipApplicationTests.java
@@ -0,0 +1,13 @@
+package intership.libraryintership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryIntershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
