c4746c2362da83b525305a278c3867ae3d0b220c
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-18 18:52:44 +0500

    Added: Main

---
+++ src/main/java/intership/libraryintership/LibraryIntershipApplication.java
@@ -0,0 +1,13 @@
+package intership.libraryintership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryIntershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryIntershipApplication.class, args);
+    }
+
+}
