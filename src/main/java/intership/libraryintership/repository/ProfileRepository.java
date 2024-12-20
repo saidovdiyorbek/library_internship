363e3693d2fa72ac6bf0d37ccb489ea8c20867ea
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-20 08:43:27 +0500

    Added: ProfileRepository

---
+++ src/main/java/intership/libraryintership/repository/ProfileRepository.java
@@ -0,0 +1,10 @@
+package intership.libraryintership.repository;
+
+import intership.libraryintership.entity.Profile;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends CrudRepository<Profile, Integer> {
+    Optional<Profile> findByUsernameAndVisibleTrue(String username);
+}
