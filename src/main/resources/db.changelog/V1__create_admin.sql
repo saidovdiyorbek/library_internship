018f2b758307a9a56633911188f3d69d74299847
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-20 20:54:56 +0500

    Change values: ADMIN to ROLE_ADMIN

--- src/main/resources/db.changelog/V1__create_admin.sql
+++ src/main/resources/db.changelog/V1__create_admin.sql
@@ -1,3 +1,3 @@
 INSERT INTO profile (name, email, username, password, created_at, visible, role, status)
-Values ('diyor', 'ddiiyyoorr009@gmail.com','admin', '$2a$12$IAZx3lzv1INguDcVoJj0AeW6.JM9O6As06x7TT2mU3VPGe1yww0Bi',NOW(), true, 'ADMIN', 'ACTIVE' );
+Values ('diyor', 'ddiiyyoorr009@gmail.com','admin2', '$2a$12$IAZx3lzv1INguDcVoJj0AeW6.JM9O6As06x7TT2mU3VPGe1yww0Bi',NOW(), true, 'ROLE_ADMIN', 'ACTIVE' );
 
