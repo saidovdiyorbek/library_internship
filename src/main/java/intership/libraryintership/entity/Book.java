279f7d431e4f1ed99c9230501a7b48add770f71c
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 07:31:15 +0500

    Added: annotation

--- src/main/java/intership/libraryintership/entity/Book.java
+++ src/main/java/intership/libraryintership/entity/Book.java
@@ -1,20 +1,30 @@
 package intership.libraryintership.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "book")
+@Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String authorId;
+
+    @Column(nullable = false)
     private String genreId;
+
+    @Column
     private Integer count;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 }
