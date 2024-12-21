f22fdf89db89363ef9e57d05de6409c53bed564d
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 07:25:02 +0500

    Added: custom exceptions

---
+++ src/main/java/intership/libraryintership/exceptions/AppBadRequestException.java
@@ -0,0 +1,7 @@
+package intership.libraryintership.exceptions;
+
+public class AppBadRequestException extends RuntimeException {
+    public AppBadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/intership/libraryintership/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package intership.libraryintership.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
