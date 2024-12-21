7426ef6b6809401af76ac02876a64460ce1001bd
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 18:43:39 +0500

    Added: Custom exception

---
+++ src/main/java/intership/libraryintership/exceptions/DuplicateDataException.java
@@ -0,0 +1,7 @@
+package intership.libraryintership.exceptions;
+
+public class DuplicateDataException extends RuntimeException {
+    public DuplicateDataException(String message) {
+        super(message);
+    }
+}
