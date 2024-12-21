2a865abdbde3f07e5e9e3b4ea0606bf5703b61f0
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 07:28:54 +0500

    Added: Loan and fields

---
+++ src/main/java/intership/libraryintership/entity/Loan.java
@@ -0,0 +1,35 @@
+package intership.libraryintership.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "loan")
+@Data
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "member_id", nullable = false)
+    private String memberId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", insertable = false, updatable = false)
+    private Member member;
+
+    @Column(name = "book_id", nullable = false)
+    private String bookId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", insertable = false, updatable = false)
+    private Book book;
+
+    @Column(nullable = false)
+    private LocalDate issueDate;
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    @Column
+    private LocalDate returnDate;
+}
