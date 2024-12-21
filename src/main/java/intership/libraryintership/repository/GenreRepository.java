d297332d8a18b5407133aeb45776dc17e51f45b5
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 19:29:15 +0500

    Added: GenreRepository

--- src/main/java/intership/libraryintership/repository/GenreRepository.java
+++ src/main/java/intership/libraryintership/repository/GenreRepository.java
@@ -3,5 +3,9 @@ package intership.libraryintership.repository;
 import intership.libraryintership.entity.Genre;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface GenreRepository extends CrudRepository<Genre, String> {
+
+    Optional<Genre> findByTitle(String genreName);
 }
