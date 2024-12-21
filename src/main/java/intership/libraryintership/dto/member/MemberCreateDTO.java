a6db4677fe68972562b561f5aa7ab42010750df1
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 22:08:31 +0500

    Added: MemberCreateDTO

---
+++ src/main/java/intership/libraryintership/dto/member/MemberCreateDTO.java
@@ -0,0 +1,29 @@
+package intership.libraryintership.dto.member;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+public record MemberCreateDTO(
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String phone,
+        @NotBlank
+        Integer createdById
+) {
+    public record MemberStandardResponse(
+            String id,
+            String name,
+            String email,
+            String phone,
+            Integer createdById,
+            LocalDateTime createAt,
+            LocalDateTime memberShipDate){}
+}
