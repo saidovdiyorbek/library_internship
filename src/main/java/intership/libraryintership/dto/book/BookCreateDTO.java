7f780a768367849a0247e6fefba206a2d9891284
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 18:42:38 +0500

    Added: BookCreateDTO and BookCreateDTO.BookStandardResponse

---
+++ src/main/java/intership/libraryintership/dto/book/BookCreateDTO.java
@@ -0,0 +1,17 @@
+package intership.libraryintership.dto.book;
+
+public record BookCreateDTO(
+        String title,
+        String authorId,
+        String genreId,
+        Integer count
+) {
+
+    public record BookStandardResponse(
+            String id,
+            String title,
+            String authorId,
+            String genreId,
+            Integer count
+    ){}
+}
