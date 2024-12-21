24286e67312e181cbfa46342cfb79ad02e60832b
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 19:29:39 +0500

    Added: GenreService

--- src/main/java/intership/libraryintership/service/GenreService.java
+++ src/main/java/intership/libraryintership/service/GenreService.java
@@ -1,15 +1,56 @@
 package intership.libraryintership.service;
 
+import intership.libraryintership.dto.genre.GenreCreateDTO;
+import intership.libraryintership.entity.Genre;
+import intership.libraryintership.mapper.GenreMapper;
 import intership.libraryintership.repository.GenreRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class GenreService {
-    private final GenreRepository genreRepository;
+    private static final Logger log = LoggerFactory.getLogger(GenreService.class);
+    private final GenreMapper mapper;
+
+    private final GenreRepository repository;
 
     public boolean exists(String genreId) {
-        return genreRepository.existsById(genreId);
+        return repository.existsById(genreId);
+    }
+
+    public GenreCreateDTO.GenreStandardResponse create(GenreCreateDTO dto) {
+        log.info("Create Genre in service: {}", dto);
+        Optional<Genre> optionalGenre = repository.findByTitle(dto.title().trim());
+
+
+        if (!optionalGenre.isPresent()) {
+            Genre genre = mapper.genreCreateDTOtoGenre(dto);
+            genre.setCreatedAt(LocalDateTime.now());
+            repository.save(genre);
+            log.info("Create Genre in service not duplicate: {}", dto);
+            return new GenreCreateDTO.GenreStandardResponse(genre.getId(), genre.getTitle(), null);
+        }
+        Genre genre = optionalGenre.get();
+        log.info("Create Genre in service already exists: {}", dto);
+        return new GenreCreateDTO.GenreStandardResponse(genre.getId(), genre.getTitle(), genre.getId());
+    }
+
+    public List<GenreCreateDTO.GenreStandardResponse> getAll() {
+        log.info("Get all Genre in service");
+        List<GenreCreateDTO.GenreStandardResponse> genres = new ArrayList<>();
+        for (Genre genre : repository.findAll()) {
+            genres.add(mapper.GenreToGenreStandardResponse(genre));
+        }
+        log.info("Get all Genre in service not duplicate: {}", genres);
+        return genres;
     }
 }
