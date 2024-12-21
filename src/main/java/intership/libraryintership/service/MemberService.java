785c30e4b815ebb42fdb379157b46441618bb613
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 22:09:06 +0500

    Added: MemberService

---
+++ src/main/java/intership/libraryintership/service/MemberService.java
@@ -0,0 +1,73 @@
+package intership.libraryintership.service;
+
+import intership.libraryintership.dto.genre.GenreCreateDTO;
+import intership.libraryintership.dto.member.MemberCreateDTO;
+import intership.libraryintership.entity.Genre;
+import intership.libraryintership.entity.Member;
+import intership.libraryintership.exceptions.DataNotFoundException;
+import intership.libraryintership.exceptions.DuplicateDataException;
+import intership.libraryintership.mapper.MemberMapper;
+import intership.libraryintership.repository.MemberRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private static final Logger log = LoggerFactory.getLogger(MemberService.class);
+    private final MemberRepository repository;
+    private final MemberMapper mapper;
+
+    public MemberCreateDTO.MemberStandardResponse create(MemberCreateDTO  dto) {
+        log.info("create member in service");
+        boolean exists = repository.existsByEmailAndPhone(dto.email(), dto.phone());
+        if (exists) {
+            throw new DuplicateDataException("Member already exists");
+        }
+
+        Member member = mapper.createDTOToMember(dto);
+        member.setCreatedAt(LocalDateTime.now());
+        member.setMemberShipDate(LocalDateTime.now());
+        repository.save(member);
+        log.info("create member success in service");
+        return new MemberCreateDTO.MemberStandardResponse(member.getId(), member.getName(), member.getEmail(), member.getPhone(), member.getCreatedById(), member.getCreatedAt(), member.getMemberShipDate());
+    }
+
+    public List<MemberCreateDTO.MemberStandardResponse> getAll() {
+        log.info("get all members in service");
+        List<MemberCreateDTO.MemberStandardResponse> members = new ArrayList<>();
+        for (Member member : repository.findAll()) {
+            members.add(mapper.memberToStandardResponse(member));
+        }
+        return members;
+    }
+
+    public MemberCreateDTO.MemberStandardResponse update(String memberId, MemberCreateDTO dto) {
+        log.info("update member in service");
+        Optional<Member> byId = repository.findById(memberId);
+        if (byId.isEmpty()) {
+            throw new DataNotFoundException("Member not found");
+        }
+        Member member = byId.get();
+        member.setName(dto.name());
+        member.setEmail(dto.email());
+        member.setPhone(dto.phone());
+        repository.save(member);
+        log.info("update member success in service");
+        return mapper.memberToStandardResponse(member);
+    }
+
+    public String delete(String memberId) {
+        log.info("delete member in service");
+        repository.findById(memberId).ifPresent(repository::delete);
+        return "Deleted "+memberId;
+    }
+}
