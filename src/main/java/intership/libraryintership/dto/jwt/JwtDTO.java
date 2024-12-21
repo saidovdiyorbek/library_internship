4182c350bbb4cd135260a55a685246ef1c565bc4
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 07:29:59 +0500

    Added: JwtDTO.TokenDTO and JwtDTO.RefreshTokenRequest

--- src/main/java/intership/libraryintership/dto/jwt/JwtDTO.java
+++ src/main/java/intership/libraryintership/dto/jwt/JwtDTO.java
@@ -1,8 +1,20 @@
 package intership.libraryintership.dto.jwt;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record JwtDTO(
         String login,
         String role
 ) {
+    public record TokenDTO(
+        String accessToken,
+        String refreshToken
+    ){}
 
+    public record RefreshTokenRequest(
+            @NotBlank(message = "Refresh token not blank")
+            String refreshToken
+    ){
+
+    }
 }
