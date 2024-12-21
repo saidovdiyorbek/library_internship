eb4310659390dcbf583067366b70a2c3f131257a
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 07:32:04 +0500

    Added: API refresh

--- src/main/java/intership/libraryintership/controller/AuthController.java
+++ src/main/java/intership/libraryintership/controller/AuthController.java
@@ -1,8 +1,10 @@
 package intership.libraryintership.controller;
 
 import intership.libraryintership.dto.auth.LoginDTO;
+import intership.libraryintership.dto.jwt.JwtDTO;
 import intership.libraryintership.dto.jwt.JwtResponseDTO;
 import intership.libraryintership.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,4 +23,10 @@ public class AuthController {
     public ResponseEntity<JwtResponseDTO> login(@RequestBody LoginDTO loginDTO) {
         return ResponseEntity.ok(service.login(loginDTO));
     }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtDTO.TokenDTO> refresh(@RequestBody @Valid JwtDTO.RefreshTokenRequest refresh) {
+        return ResponseEntity.ok(service.refresh(refresh));
+    }
+
 }
