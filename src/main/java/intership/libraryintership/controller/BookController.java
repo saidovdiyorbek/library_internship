50b739d812d8c67ac98f2bc98a13a418a2de7fa4
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 18:42:19 +0500

    Added: BookController

---
+++ src/main/java/intership/libraryintership/controller/BookController.java
@@ -0,0 +1,38 @@
+package intership.libraryintership.controller;
+
+import intership.libraryintership.dto.book.BookCreateDTO;
+import intership.libraryintership.service.BookService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/book")
+@RequiredArgsConstructor
+public class BookController {
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+    private final BookService service;
+
+    @PostMapping("/create")
+    public ResponseEntity<BookCreateDTO.BookStandardResponse> create(@RequestBody @Valid BookCreateDTO dto){
+        logger.info("create book");
+        return ResponseEntity.ok(service.create(dto));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<BookCreateDTO.BookStandardResponse>> getAll(){
+        logger.info("getAll book");
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @PutMapping("/update/{bookId}")
+    public ResponseEntity<BookCreateDTO.BookStandardResponse> update(@PathVariable("bookId") String bookId, @RequestBody @Valid BookCreateDTO dto){
+        logger.info("update book");
+        return ResponseEntity.ok(service.update(bookId, dto));
+    }
+}
