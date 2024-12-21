fe2dcf91885041e3b9ef84055e6b3161c4a7bddc
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-21 07:25:30 +0500

    Added: SpringSecurityUtil and method getCurrentUserId

---
+++ src/main/java/intership/libraryintership/util/SpringSecurityUtil.java
@@ -0,0 +1,21 @@
+package intership.libraryintership.util;
+
+import intership.libraryintership.config.CustomUserDetails;
+import intership.libraryintership.exceptions.AppBadRequestException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SpringSecurityUtil {
+    public static Integer getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof CustomUserDetails) {
+                return ((CustomUserDetails) principal).getId();
+            } else if (principal instanceof String && "anonymousUser".equals(((String) principal).trim())) {
+                throw new RuntimeException("Anonymous users can't be authenticated");
+            }
+        }
+        throw new AppBadRequestException("http://localhost:8090/api/auth/login");
+    }
+}
