b8e8a78e7af5a1f47c6bbc8f67ea7bb0ac7464b9
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-22 06:41:57 +0500

    Added: AuthorMapper

--- src/main/java/intership/libraryintership/mapper/author/AuthorMapper.java
+++ src/main/java/intership/libraryintership/mapper/author/AuthorMapper.java
@@ -1,10 +1,10 @@
-package intership.libraryintership.mapper;
+package intership.libraryintership.mapper.author;
 
 import intership.libraryintership.dto.author.AuthorCreateDTO;
 import intership.libraryintership.entity.Author;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AuthorMapper {
     AuthorCreateDTO.AuthorStandardResponse authorToAuthorStandardResponse(Author author);
     Author authorCreateDTOToAuthor(AuthorCreateDTO authorCreateDTO);
